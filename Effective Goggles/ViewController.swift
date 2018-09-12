b1e0635d5b7459ea8e663a50455c35be43c32f83
Yasha Mostofi <yasha.mostofi@gmail.com>
2018-09-12 17:45:24 -0500

    Initial Commit

---
+++ Effective Goggles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Effective Goggles
+//
+//  Created by Yasha Mostofi on 9/12/18.
+//  Copyright © 2018 Yasha Mostofi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
